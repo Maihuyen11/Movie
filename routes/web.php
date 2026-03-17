e755b6d6c856f7bd571698a4c28503d6003357e7
Maihuyen11 <030239230076@st.buh.edu.vn>
2026-03-17 10:14:25 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::get('/', function () {
 // Route for TestController@test
 Route::get('/test', [\App\Http\Controllers\TestController::class, 'test']);
 
+Route::get('/buimaihuyen', function () {
+    return 'Bùi Mai Huyền';
+});
