a45bf08f9dbe734463b63b1ed6873d198a9210ec
Thu Hung <tranvothuhung@gmail.com>
2026-03-17 19:06:54 +0700

    Hoan thanh yeu cau 7.1

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB; 
 
 Route::get('/baoyen', function () {
-    return view('welcome');
+    return 'Bảo Yến'; 
 });
 
 // Route for TestController@test
@@ -11,4 +12,11 @@ Route::get('/test', [\App\Http\Controllers\TestController::class, 'test']);
 
 Route::get('/buimaihuyen', function () {
     return 'Bùi Mai Huyền';
+});
+
+Route::get('/the-loai-phim', function () {
+    
+    $genres = DB::table('genre')->get(); 
+    
+    return view('genres', compact('genres')); 
 });
